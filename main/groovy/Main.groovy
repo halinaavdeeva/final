e5d7a482e663307ca20a562ff9137de6960cd815
halinaavdeeva <65125535+halinaavdeeva@users.noreply.github.com>
2020-06-10 23:04:57 +0300

    Add files via upload

---
+++ main/groovy/Main.groovy
@@ -0,0 +1,31 @@
+import geb.Browser
+
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Halina_Avdeeva\\Desktop\\DQE_Mentoring\\Final_Task\\chromedriver.exe")
+
+Browser.drive {
+
+    dlab = { page DLab }
+    jupyter = { page Jupyter }
+    jupyterNotebook = { page JupyterNotebook }
+
+    to DLab
+    dlab.epamSSOButton.click()
+
+    at Jupyter
+    waitFor() { jupyter.folder.displayed }
+    jupyter.folder.click()
+
+    ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+    driver.switchTo().window(tabs.get(1));
+    driver.get("https://projectby.trainings.dlabanalytics.com/havdeeva89/notebooks/Required_checks.ipynb")
+
+    at JupyterNotebook
+
+    jupyterNotebook.cellButton.click()
+    jupyterNotebook.runAllActionItem.click()
+
+    waitFor() { $("div.output_subarea.output_html.rendered_html.output_result").displayed }
+
+}
+
+
